5602bb5649444b9ff2c0b8e4a6b97f7dfe0217c4
Yoann Fleury <yoann.fleury@yahoo.com>
2018-10-27 18:50:16 +0200

    docs: change dist parameters

--- config/parameters.dist.ts
+++ config/parameters.dist.ts
@@ -1,4 +1,10 @@
 export default {
-  ip: "192.168.x.x",
-  user: "phillips-hue-user"
+  hue: {
+    ip: "192.168.x.x",
+    user: "phillips-hue-user"
+  },
+  weather: {
+    base: "api.openweathermap.org/data/2.5",
+    key: "key"
+  }
 };
